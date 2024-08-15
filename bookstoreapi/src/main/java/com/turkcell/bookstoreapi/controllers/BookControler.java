6bad917fdbcb642fd8b8572d7f658e8cbb18dd52
neominal.solutions <neominal.solutions@gmail.com>
2024-08-15 07:14:44 +0000

    BookController

--- bookstoreapi/src/main/java/com/turkcell/bookstoreapi/controllers/BookControler.java
+++ bookstoreapi/src/main/java/com/turkcell/bookstoreapi/controllers/BookControler.java
@@ -1,18 +1,25 @@
 package com.turkcell.bookstoreapi.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.turkcell.bookstoreapi.dtos.BookDto;
 import com.turkcell.bookstoreapi.services.BookService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 
@@ -35,6 +42,32 @@ public class BookControler {
 
         return new ResponseEntity<>(book,HttpStatus.CREATED);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BookDto> getBookById(@PathVariable String id) {
+        var book = bookService.getBookById(id);
+        return book.map(ResponseEntity::ok).orElseGet(()-> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<BookDto> putBook(@PathVariable String id, @RequestBody BookDto request) {
     
+        var response = bookService.updateBook(id, request);
+        if(response != null) {
+            return new ResponseEntity<>(response,HttpStatus.OK);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<BookDto> deleteBook(@PathVariable String id) {
+    
+        var isOk = bookService.deleteBook(id);
+
+        if(isOk) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return ResponseEntity.notFound().build();
+    }
     
 }
